3aecf7bed4394b256115c4ee61f6668b9460d7da
Thayssa Afonso Souza <105455780+thayssa-souza@users.noreply.github.com>
2022-09-22 12:59:24 -0300

    Projeto iniciado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Header from './Components/Header';
+import Main from './Components/Main';
+import Footer from './Components/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <section>
+    <div>
+      <Header />
     </div>
+    <div>
+      <Main />
+    </div>
+    <div>
+      <Footer />
+    </div>
+    </section>
   );
 }
 
---
+++ src/Styles/mainStyle.js
@@ -0,0 +1,6 @@
+import React, { useState, useEffect, useRef } from "react";
+import styled, { createGlobalStyle, keyframes } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+`
