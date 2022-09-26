5b81aae52cb6b0312b4b417e0d5377b22fe530eb
Thayssa Afonso Souza <105455780+thayssa-souza@users.noreply.github.com>
2022-09-25 21:07:36 -0300

    Created Main

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,8 +1,53 @@
 import React, { useState, useEffect, useRef } from "react";
 import styled, { createGlobalStyle, keyframes } from "styled-components";
+import * as S from '../Styles/mainStyle'
+import CakeRedVelvet from './img/CakeRedVelvet.png'
+import Pizza from './img/Pizza.png'
+import Smoothie from './img/Smoothie.png'
+import Spoon from './img/Spoon.png'
 
 export default function Main() {
   return (
-    <div>Main</div>
+    <S.Container>
+      <S.ContainerTitle>
+        <S.Title>LATEST RECIPES</S.Title>
+        <S.UnderScore></S.UnderScore>
+      </S.ContainerTitle>
+      <S.CntImg>
+      <S.ContainerImage>
+        <S.Imagem src={CakeRedVelvet} alt='Bolo red velvet' />
+        <S.TextBox>
+          <S.TextUnderScore></S.TextUnderScore>
+          <S.Text>Red Velvet Cake</S.Text>
+        </S.TextBox>
+      </S.ContainerImage>
+
+      <S.ContainerImage>
+        <S.Imagem src={Pizza} alt='Pizza margherita' />
+        <S.TextBox>
+          <S.TextUnderScore></S.TextUnderScore>
+          <S.Text>Margherita Pizza</S.Text>
+        </S.TextBox>
+      </S.ContainerImage>
+
+      <S.ContainerImage>
+        <S.Imagem src={Smoothie} alt='Dois Smoothies' />
+        <S.TextBox>
+          <S.TextUnderScore></S.TextUnderScore>
+          <S.Text>Peanut Smoothie</S.Text>
+        </S.TextBox>
+      </S.ContainerImage>
+      </S.CntImg>
+      <S.Div></S.Div>
+
+      <S.FinalImage>
+        <S.SpoonImg src={Spoon} alt='A spoon with himalayan salt' />
+        <S.FinalImgText>
+          <S.FinalImgTitle>ABOUT</S.FinalImgTitle>
+          <S.TextUnderScore></S.TextUnderScore>
+          <S.P>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tincidunt, tortor nec rhoncus dictum, lorem massa tempus sem, eu tincidunt libero velit sit amet velit. Nunc in euismod urna. Duis dapibus, elit eu eleifend tincidunt, nulla ipsum consectetur lorem, quis tempor lorem justo quis nisi. Nam interdum, nisi nec mollis sagittis, enim risus euismod nisi, quis rutrum quam augue id mauris. Pellentesque mattis hendrerit semper. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut vestibulum nisl ante, et ultricies sapien facilisis aliquam.</S.P>
+        </S.FinalImgText>
+      </S.FinalImage>
+    </S.Container>
   )
 }
--- src/Styles/mainStyle.js
+++ src/Styles/mainStyle.js
@@ -1,3 +1,100 @@
 import React, { useState, useEffect, useRef } from "react";
 import styled, { createGlobalStyle, keyframes } from "styled-components";
 
+export const Container = styled.section`
+    background-color:#F2F4F1;
+    color:#373737;
+`
+
+export const ContainerTitle = styled.div`
+    height:50vh;
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items:center;
+`
+
+export const Title = styled.h2`
+    font-size: 40px;
+`
+export const UnderScore = styled.h2`
+    width:9vw;
+    border:solid;
+    margin-top:5vh;
+`
+
+export const CntImg = styled.section`
+    display:flex;
+    justify-content: space-evenly;
+`
+
+export const ContainerImage = styled.section`
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items:center;
+`
+
+export const Imagem = styled.img`
+    width:25vw;
+    height:45vh;
+    margin-bottom:3vh;
+`
+
+export const TextBox = styled.div`
+    margin-top:-3vh;
+    width:25vw;
+    height:45vh;
+    border: solid #FFFFFF;
+    border-right-width: 0.1vw;
+    border-top-width:0.1vw;
+    border-bottom-width:0vw;
+    display:flex;
+    flex-direction:column;
+    justify-content: center;
+    align-items: center;
+`
+
+export const TextUnderScore = styled.h2`
+    border:solid;
+    width:3vw;
+    margin-bottom:4vh;
+`
+
+export const Text = styled.h3`
+    font-size:20px;
+    font-weight: 300;
+`
+
+export const Div = styled.div`
+    height:30vh;
+`
+
+export const FinalImage = styled.div`
+    width:100%;
+    display:flex;
+`
+
+export const SpoonImg = styled.img`
+    width:50%;
+`
+
+export const FinalImgText = styled.div`
+    width:50%;
+    background-color: #FFFFFF;
+    display:flex;
+    flex-direction:column;
+    justify-content: center;
+    align-items: center;
+`
+
+export const FinalImgTitle = styled.h3`
+    font-size: 40px;
+    margin-bottom: 10vh;
+`
+
+export const P = styled.p`
+    width:35vw;
+    text-align: justify;
+    line-height:25px;
+`
