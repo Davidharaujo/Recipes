51b3a2479f417c650ad2aa54ae966de1ff83a654
Thayssa Afonso Souza <105455780+thayssa-souza@users.noreply.github.com>
2022-09-26 13:37:30 -0300

    Update background-image and logo on header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect, useRef } from "react";
 import styled, { createGlobalStyle, keyframes } from "styled-components";
 import * as S from '../Styles/headerStyle'
+import Logo from './img/Logo-rc.png'
 
 export default function Header() {
   return (
     <S.Container>
         <S.Header>
-            <img src="https://cdn-sharing.adobecc.com/content/storage/id/urn:aaid:sc:US:e048e30a-811b-49f2-a92f-1af9a2985abd;revision=0?component_id=96b0b497-674d-472a-89e9-aa680a0712af&api_key=CometServer1&access_token=1663906651_urn%3Aaaid%3Asc%3AUS%3Ae048e30a-811b-49f2-a92f-1af9a2985abd%3Bpublic_1fc7cd0d46a433289c1dbdfeb39ef97c3c4d141c" alt="Logo" />
+            <img src={Logo} alt="Logo" />
             <S.Navigation>
                 <S.Lista>
                     <S.Item>ABOUT</S.Item>
--- src/Styles/headerStyle.js
+++ src/Styles/headerStyle.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect, useRef } from "react";
 import styled, { createGlobalStyle, keyframes } from "styled-components";
+import background from '../Components/img/background.png'
 
 export const Container = styled.section`
-    background-image: url('https://cdn-sharing.adobecc.com/content/storage/id/urn:aaid:sc:US:e048e30a-811b-49f2-a92f-1af9a2985abd;revision=0?component_id=8444393a-5d93-4997-8ad0-0d450a8011e0&api_key=CometServer1&access_token=1663906651_urn%3Aaaid%3Asc%3AUS%3Ae048e30a-811b-49f2-a92f-1af9a2985abd%3Bpublic_1fc7cd0d46a433289c1dbdfeb39ef97c3c4d141c');
+    background-image: url(${background});
     background-repeat: no-repeat;
     background-size: 100vw 100vh;
     display: flex;
